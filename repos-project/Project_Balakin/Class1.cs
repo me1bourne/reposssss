e6f652b231caf35bc6617cc960a0c1bfa89a76b1
me1bourne <95063789+me1bourne@users.noreply.github.com>
2023-04-05 02:03:08 +0500

    Add files via upload

---
+++ repos-project/Project_Balakin/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Project_Balakin
+{
+    public static class ClassFrame1
+    {
+        static public Frame frame1 = new Frame();
+    }
+}
